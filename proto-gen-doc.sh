61eddd526bec5b2a98c2520c2af90f8208e30712
Katsutoshi Hayashi <ezio1119@gmail.com>
2020-05-19 18:34:34 +0900

    setup github actions

---
+++ proto-gen-doc.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+docker run --rm -v ${PWD}:/proto ezio1119/protoc \
+-I/proto \
+--doc_out=. \
+--doc_opt=markdown,README.md \
+chat/chat.proto \
+event/event.proto \
+auth/auth.proto \
+post/post.proto \
+profile/profile.proto
