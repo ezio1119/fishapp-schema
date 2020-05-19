7f6859728e8c8874a3483141e5a1c416c97172c2
Katsutoshi Hayashi <ezio1119@gmail.com>
2020-05-19 18:41:44 +0900

    setup github actions

--- proto-gen-doc.sh
+++ proto-gen-doc.sh
@@ -2,6 +2,7 @@
 
 docker run --rm -v ${PWD}:/proto ezio1119/protoc \
 -I/proto \
+-I/go/src/github.com/envoyproxy/protoc-gen-validate \
 --doc_out=. \
 --doc_opt=markdown,README.md \
 chat/chat.proto \
